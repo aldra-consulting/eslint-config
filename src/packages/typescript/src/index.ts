dbeb472fe1e9300a9215d648901ceee1232f6613
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-16 14:56:40 +0200

    chore(packages/typescript): add reusable TypeScript ESLint configuration

---
+++ src/packages/typescript/src/index.ts
@@ -0,0 +1,67 @@
+import base from '@aldra-consulting/eslint-config-base';
+
+import { type Configuration } from '@project/types';
+
+const configuration: Configuration = {
+  ...base,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  extends: [
+    ...base.extends,
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:import/typescript',
+  ],
+  plugins: [...base.plugins, '@typescript-eslint', 'deprecation'],
+  settings: {
+    ...base.settings,
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    ...base.rules,
+    'deprecation/deprecation': 'warn',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
+    ],
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': [
+      'error',
+      { allow: ['arrowFunctions'] },
+    ],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+  },
+};
+
+export = configuration;
