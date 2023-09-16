91c8fdc8c4c0e59b559b7280e2c22cf233742866
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-09-16 15:09:12 +0200

    chore(packages/typescript-react): add reusable TypeScript React ESLint configuration

---
+++ src/packages/typescript-react/src/index.ts
@@ -0,0 +1,42 @@
+import base from '@aldra-consulting/eslint-config-typescript';
+
+import { type Configuration } from '@project/types';
+
+const configuration: Configuration = {
+  ...base,
+  extends: [...base.extends, 'airbnb', 'airbnb/hooks'],
+  plugins: [...base.plugins, 'jsx-a11y', 'react', 'react-hooks'],
+  rules: {
+    ...base.rules,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
+    'react/jsx-no-bind': 'off',
+  },
+};
+
+export = configuration;
